b4cbd6f15816d42a3eb5d4550a06270098fafc87
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-09-16 02:12:00 +0200

    improved startup scripts

--- run-test-deployment.sh
+++ run-test-deployment.sh
@@ -4,7 +4,11 @@
 kind delete cluster --name testcluster
 kind create cluster --name=testcluster --config=./kind/config.yaml
 
+sleep 10s
+
 kubectl apply -f ./kubernetes-kafka/00-namespace.yml
+
+sleep 10s
 kubectl apply -k ./kubernetes-kafka/variants/my-dev
 
 kubectl label nodes testcluster-worker monitor-patient-data=true
@@ -13,4 +17,7 @@ kubectl label nodes testcluster-worker3 monitor-patient-data=true
 kubectl label nodes testcluster-worker4 monitor-patient-data=true
 kubectl label nodes testcluster-worker5 monitor-patient-data=true
 
+bash kubectl/deploy.sh
+
 bash run-pipeline.sh
+#kubectl get pod -o=custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.spec.nodeName -n kafka -w
