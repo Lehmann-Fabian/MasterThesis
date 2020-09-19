95e22f386cfe5844cea5f417a7ba97a844709dc8
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-09-19 21:36:38 +0200

    updated for real deployment

--- run-real-deployment.sh
+++ run-real-deployment.sh
@@ -5,22 +5,26 @@ cd MockFog2
 bash start.sh
 cd ..
 
-
 python3 generateKubernetesHosts.py ./MockFog2/node-manager/run/machines/machine_meta.jsonc kubespray/inventory/mycluster/hosts.yaml
 
-exit 0
 bash startKubernetes.sh
+
+export KUBECONFIG=~MasterThesisCode/kubespray/inventory/mycluster/artifacts/admin.conf
+
+kubectl apply -f storage/default-storage.yml
+
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 
 kubectl apply -f ./kubernetes-kafka/00-namespace.yml
 
 sleep 10s
 kubectl apply -k ./kubernetes-kafka/variants/my-dev
 
-kubectl label nodes testcluster-worker monitor-patient-data=true
-kubectl label nodes testcluster-worker2 monitor-patient-data=true
-kubectl label nodes testcluster-worker3 monitor-patient-data=true
-kubectl label nodes testcluster-worker4 monitor-patient-data=true
-kubectl label nodes testcluster-worker5 monitor-patient-data=true
+kubectl label nodes server1 monitor-patient-data=true
+kubectl label nodes server2 monitor-patient-data=true
+kubectl label nodes server3 monitor-patient-data=true
+#kubectl label nodes server4 monitor-patient-data=true
+#kubectl label nodes server5 monitor-patient-data=true
 
 cd kubectl
 bash deploy.sh
@@ -29,7 +33,4 @@ cd ..
 bash run-pipeline.sh
 #kubectl get pod -o=custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.spec.nodeName -n kafka -w
 
-sleep 5m
-docker stop testcluster-worker6
-sleep 15m
-docker stop testcluster-worker7
+exit 0
