9922355b9071ca98d27674c1f21ea2486a9ce877
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-09-26 14:00:27 +0200

    log responsible nodes for pods and topics

---
+++ run-real-experiment.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "install a default pvc provisioner"
+kubectl apply -f storage/default-storage.yml
+
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+echo "install kafka"
+kubectl apply -f ./kubernetes-kafka/00-namespace.yml
+
+sleep 10s
+kubectl apply -k ./kubernetes-kafka/variants/my-dev
+
+
+echo "which nodes are next to patients"
+kubectl label nodes server1 monitor-patient-data=true
+kubectl label nodes server2 monitor-patient-data=true
+kubectl label nodes server3 monitor-patient-data=true
+kubectl label nodes server4 monitor-patient-data=true
+kubectl label nodes server5 monitor-patient-data=true
+kubectl label nodes server6 monitor-patient-data=true
+kubectl label nodes server7 monitor-patient-data=true
+
+echo "Start observation tool"
+cd kubectl
+bash deploy.sh
+cd ..
+
+#launch pipeline services
+bash run-pipeline.sh
+#kubectl get pod -o=custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.spec.nodeName -n kafka -w
+
+#start logging
+pid=`nohup bash fetchResponsibilities.sh >> logs/responsibilities.log & echo $!`
+
+#start experiment manipulation
+cd MockFog2
+bash run-orchestration.sh
+cd ..
+
+sleep 7m
+
+kill $pid
+
+exit 0
