1ee64fbfb015ea6ebcec01deacfed413fe812d75
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-10-21 16:55:33 +0200

    run experiments with timeouts

--- run-one-experiment.sh
+++ run-one-experiment.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-bash run-real-deployment.sh $1
+
+
+timeout --foreground 35m bash <<EOT
+    bash run-real-deployment.sh $1
+EOT
+
+code=$?
+
+if [[ "$code" -ne 0 ]]; then
+    cd MockFog2
+    bash destroy.sh
+    cd ..
+    exit 124
+
+fi
 
 sleep 20s
 
-bash run-real-experiment.sh $1 $2
+timeout --foreground 50m bash <<EOT
+    bash run-real-experiment.sh $1 $2
+EOT
+
+code=$?
+
+if [[ "$code" -ne 0 ]]; then
+    cd MockFog2
+    bash destroy.sh
+    cd ..
+    exit 124
+
+fi
 
 sleep 20s
 
 cd MockFog2
-
 bash destroy.sh
-
 cd ..
