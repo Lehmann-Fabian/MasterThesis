b45affee492dc6b48157ae8bba6722ec8eb861ab
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-11-04 12:46:56 +0100

    wait for destroy

--- run-one-experiment.sh
+++ run-one-experiment.sh
@@ -35,6 +35,16 @@ fi
 sleep 20s
 
 cd MockFog2
-bash destroy.sh
+
+code=124
+while [[ "$code" -eq 124 ]]; do
+
+timeout --foreground 5m bash <<EOT
+    bash destroy.sh
+EOT
+code=$?
+
+done
+
 cd ..
 exit 0
